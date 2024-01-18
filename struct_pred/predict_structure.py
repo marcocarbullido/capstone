bbefafc4462a77216ab8ca61655734bc90626631
marcocarbullido <124408893+marcocarbullido@users.noreply.github.com>
2024-01-18 16:20:16 -0600

    Update predict_structure.py

--- struct_pred/predict_structure.py
+++ struct_pred/predict_structure.py
@@ -1,3 +1,3 @@
-#TODO: Insert code to retrieve all PDB files from capstone/input_files/ and write to localcolabfold/input_fasta.
+#TODO: Insert code to retrieve all PDB files from capstone/struct_pred/input_files/ and write to localcolabfold/input_fasta.
 #TODO: Script to execute localcolabfold and save outputs
 #TODO: Write relevant results to capstone/struct_pred/results/PROTEIN_NAME/AF-PROTEIN_NAME.pdb. Also save original PDB there too.
